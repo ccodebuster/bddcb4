f73c6f144ac84308119e6a25bca7a8a3e9406157
bhav3583 <bhavesh3583@yahoo.co.in>
2023-12-21 20:23:37 +0000

    thursday

--- src/main/java/com/nopcommerce/driverfactory/ManageDriver.java
+++ src/main/java/com/nopcommerce/driverfactory/ManageDriver.java
@@ -1,9 +1,9 @@
 package com.nopcommerce.driverfactory;
 
 import com.nopcommerce.propertyreader.PropertyReader;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
--- src/main/java/com/nopcommerce/pages/LoginPage.java
+++ src/main/java/com/nopcommerce/pages/LoginPage.java
@@ -1,8 +1,8 @@
 package com.nopcommerce.pages;
 
 import com.nopcommerce.utility.Utility;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
